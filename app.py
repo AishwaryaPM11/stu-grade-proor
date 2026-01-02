981a31084eef3e754e8595a32ba4fccff46dc057
AishwaryaPM11 <aishwaryamulagund0@gmail.com>
2026-01-02 23:24:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
 import sys
 
+def calculate_grade(avg):
+    if avg >= 90:
+        return "S"
+    elif avg >= 80:
+        return "A"
+    elif avg >= 65:
+        return "B"
+    elif avg >= 50:
+        return "C"
+    elif avg >= 40:
+        return "D"
+    else:
+        return "F"
+
 def main():
-   
     if len(sys.argv) > 1:
         name = sys.argv[1]
     else:
         name = "Student"
 
-    
-    department = "CSE"
-    semester = 5
     m1, m2, m3 = 70, 80, 90
-
     avg = (m1 + m2 + m3) / 3
+    grade = calculate_grade(avg)
 
-   
-    if avg >= 90:
-        grade = "S"
-    elif avg >= 80:
-        grade = "A"
-    elif avg >= 65:
-        grade = "B"
-    elif avg >= 50:
-        grade = "C"
-    elif avg >= 40:
-        grade = "D"
-    else:
-        grade = "F"
-
-    # Step 4: Output
     print("Name:", name)
     print("Average:", avg)
     print("Grade:", grade)
